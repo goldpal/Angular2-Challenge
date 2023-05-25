b51f78b2dda0c8e0f3a64704b81fcfcc658e973d
goldpal <niclas.kato92@gmail.com>
2023-05-25 12:59:55 +0900

    Finished Challenge

---
+++ app/main.js
@@ -0,0 +1,13 @@
+System.register([], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    return {
+        setters:[],
+        execute: function() {
+        }
+    }
+});
+// Our main component
+//import { AppComponent } from './app.component';
+//bootstrap(AppComponent); 
+//# sourceMappingURL=main.js.map
